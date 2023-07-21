a56b0229a3c5d68cd97d7228f2927340132a0f6d
Yassin-567 <83673138+Yassin-567@users.noreply.github.com>
2023-07-21 15:24:51 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 import logging
 import os
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.types import InputFile
 from moviepy.editor import VideoFileClip
 
 # Set up logging
@@ -9,16 +10,28 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+# Initialize the bot and dispatcher
+# Replace 'YOUR_BOT_TOKEN' with your actual bot token
+bot_token = '5909482823:AAGNtdionMCBuhdfZXD4e9sPAiCH25gg0ko'
+bot = Bot(token=bot_token)
+dispatcher = Dispatcher(bot)
+dp = dispatcher  # Shorter alias for the dispatcher
+
+# Middleware to log messages
+dp.middleware.setup(LoggingMiddleware())
+
 # Define the function to handle /start command
-def start(update: Update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello! Send me a video, and I'll compress it for you.")
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    await message.answer("Hello! Send me a video, and I'll compress it for you.")
 
 # Define the function to handle video messages
-def handle_video(update: Update, context):
-    video = update.message.video
-
+@dp.message_handler(content_types=types.ContentTypes.VIDEO)
+async def handle_video(message: types.Message):
     # Get the file path of the video on Telegram's servers
-    file_path = context.bot.get_file(video.file_id).file_path
+    video = message.video
+    file_info = await bot.get_file(video.file_id)
+    file_path = file_info.file_path
 
     # Perform video compression using MoviePy
     clip = VideoFileClip(file_path)
@@ -32,35 +45,13 @@ def handle_video(update: Update, context):
     clip.write_videofile(compressed_file, codec=codec, bitrate=bitrate)
 
     # Send the compressed video
-    context.bot.send_video(chat_id=update.effective_chat.id, video=open(compressed_file, 'rb'))
+    with open(compressed_file, 'rb') as video_file:
+        await message.answer_video(InputFile(video_file))
 
     # Clean up the temporary files
     clip.close()
     os.remove(compressed_file)
 
-# Set up the Telegram bot
-def main():
-    # Replace 'YOUR_BOT_TOKEN' with your actual bot token
-    bot_token = '5909482823:AAGNtdionMCBuhdfZXD4e9sPAiCH25gg0ko'
-
-    # Initialize the bot
-    updater = Updater(token=bot_token, use_context=True)
-    dispatcher = updater.dispatcher
-
-    # Add command handlers
-    start_handler = CommandHandler('start', start)
-    dispatcher.add_handler(start_handler)
-
-    # Add message handlers
-    video_handler = MessageHandler(Filters.video, handle_video)
-    dispatcher.add_handler(video_handler)
-
-    # Start the bot in polling mode
-    updater.start_polling()
-    logger.info("Bot started!")
-
-    # Run the bot until you press Ctrl-C
-    updater.idle()
-
 if __name__ == '__main__':
-    main()
+    from aiogram import executor
+    executor.start_polling(dp, skip_updates=True)
